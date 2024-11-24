ef2b8a8bdb81a58fc2abe38ccc6e0162f0bcc183
ayachibadr <49597400+ayachibadr@users.noreply.github.com>
2024-11-24 19:55:07 +0100

    Initial commit from your app

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+module.exports = {
+      site: 'src',
+      output: 'dist',
+      integrations: {
+        astro: {
+          components: 'src/components',
+          layouts: 'src/layouts',
+          pages: 'src/pages'
+        }
+      }
+    }
